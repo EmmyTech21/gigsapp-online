fab33c5bbade709926f1b33f11f22edea8c03552
EmmyTech21 <oyelakinbusayo8@gmail.com>
2024-11-01 11:58:17 +0100

    routes changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@
 import './App.css'
 import AllRoutes from './Routes/AllRoutes'
 import './styles/index.css'
-
+import HeroSection from "./components/HeroSection";
+import TaskCategories from "./components/TaskCategories";
+import Navbar from "./components/Navbar";
+import Stats from "./Layout/Stats";
+import Service from "./Layout/Service";
+import TasksNearYou from "./components/TasksNearYou";
+import HowItWorks from "./components/HowItWorks";
+import Testimonials from "./Layout/Testimonials";
+import Footer from "./components/Footer";
 
 
 const App: React.FC = () => {
@@ -14,6 +22,20 @@ const App: React.FC = () => {
 
       <>
       <AllRoutes />
+       <Navbar />
+    <HeroSection />
+    <TaskCategories />
+    <HowItWorks />
+    <Stats/>
+    <Service />
+  
+  <TasksNearYou />
+  
+  <Testimonials/>
+  <div className="min-h-screen flex flex-col justify-between">
+    <Footer/>
+</div>
+
       </>
           
       
